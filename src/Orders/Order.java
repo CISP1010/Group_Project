e7489b3f571ebac53be039494a97b550efce497f
gclark2 <gclark2@my.chattanoogastate.edu>
2023-04-18 09:55:00 -0400

    jumbled so far today...

--- src/Orders/Order.java
+++ src/Orders/Order.java
@@ -1,34 +1,71 @@
 package Orders;
 
-import Menu.MenuItem;
-
+//gp through and change orderItem and orderItems
 public class Order {
-    private double order;
-    private int tableNum;
-    private int seatNum;
-    private String foodItem;
-    private MenuItem menuItem;
-    public Order(double order, MenuItem menuItem, int tableNum, int seatNum) {
-        this.order = order;
-        this.tableNum = tableNum;
-        this.seatNum = seatNum;
-        this.menuItem = menuItem;
-        //menuItem is the object created from MenuItem() in MenuItem.java file
+    private int orderNum = 0;
+    private String custName;
+    private String orderItem;
+
+    private String orderType;
+
+    ArrayList
+    ///add enum or array for delivery, pick up, dine in... dine in needs to trigger a table/seat assignment
+   // then choice - 1 in the employee menu thing determines the entry and if 3 -> then enter a table and seat assignment else {}
+
+
+    // default constructor
+
+    public Order(){
+
     }
 
-    public int getTableNum(){
-        return tableNum;
+    // overloaded constructor for order with params for number, customer name, items in order, dine-in/delivery
+
+
+    public Order(Int orderNum, String custName, Arraylist<String> orderItems, String orderType) {
+        this.orderNum = orderNum;;
+        this.custName = custName;
+        this.orderItem = orderItems;
+        this.orderType = orderType;
     }
 
-    public int getSeatNum() {
-        return seatNum;
+    //setters
+    public void setNum(int orderNum) {
+        this.orderNum = orderNum;
     }
 
-    public void setTableNum(int tableNum) {
-        this.tableNum = tableNum;
+    public void setCustName(String custName) {
+        this.custName = custName;
     }
 
-    public void setSeatNum(int seatNum) {
-        this.seatNum = seatNum;
+    public void setOrderItem(String orderItem){
+        this orderItem = orderItem;
     }
+
+    public void setOrderType(String orderType){
+        this.orderType = orderType;
+    }
+
+    //getters
+
+    public int getNum(){
+        return orderNum;
+    }
+
+    public String getCustName(){
+        return custName;
+    }
+
+    public String getOrderItems(){ //change this to spit out the arraylist orderitems...
+        return orderItem;
+    }
+    public String getOrderType(){
+        return orderType;
+    }
+
+    public int assignNum(){
+        orderNum++;
+        return orderNum;
+    }
+
 }
--- src/Orders/OrderMenu.java
+++ src/Orders/OrderMenu.java
@@ -1,7 +1,128 @@
 package Orders;
 
+//check for when you've referenced 'orderData' (hashmap containing integer key and order object)
+// and 'orderItems'  Array list containing the food items user has entered in order...
+import Helpers.Cls;
+import Helpers.YesNo;
+import java.util.*;
+
+import static java.lang.System.*;
 public class OrderMenu {
-    public static void main(String[] args) {
-        System.out.println("Filler Test Data");
-    }
-}
+    package Orders;
+
+    public class OrderMenu {
+        public static void main(String[] args){
+            Scanner input = new Scanner(System.in);
+            OrderData orderData = new OrderData();  //going to try to change my arraylist conception to match
+            int choice = 0;                         //the other hash stuff
+            int orderNum = 1;
+
+            String noSeat = "No seat";//I might have caused an issue by using 'choice' as a var inside loops..maybe not though
+            String noTable = "No table";
+
+            Boolean continueEntry = true;
+            do {
+              //clears screen
+                Cls.cls();
+                //Displays the order menu options
+                    System.out.println("What would you like to do?\n");
+                    System.out.println("Order Options:");
+                    System.out.println("1 - Create a new order.");
+                    System.out.println("2 - Modify an existing order.");
+                    System.out.println("3 - List all orders.");
+                    System.out.println("4 - Delete an order."); // maybe mark an order as completed too?
+                    System.out.println("5 - Return to the main menu.\n");
+                    System.out.println("Enter your selection [1,2,3,4,5]: ");
+                    //gets the user choice
+                    choice = input.nextInt();
+                    input.nextLine();
+                    //checks and proceeds with the user choice
+                    switch (choice) {
+                        case 1 -> {
+                            Cls.cls();
+                            //starts the order enterin'
+                            boolean restart;
+                            do {
+                                Cls.cls();
+                                out.println("Enter the customer's last name.");
+                                out.println("[last name]: ");
+                                String custName = input.nextLine();
+                                Cls.cls();
+                                out.println("Is this order for (1) Dine-in, (2) Delivery, or (3) Pick up?");
+                                out.println("[order type]: ";
+                                String orderType;
+                               // orderChoice takes the number to avoid confusion on my part...
+                                int orderChoice = input.nextInt();
+                                if (orderChoice == 1) {
+                                    orderType = "Dine-in"}
+                                else if (orderChoice == 2) {
+                                    orderType = "Delivery"}
+                                else {
+                                    orderType = "Pick up"}  // could add a section here to assign table/seat if dine-in later
+                                Cls.cls();
+
+                                boolean restart;
+                                 do {
+                                  int i = 1
+
+
+                                    out.println("Please enter menu item.");
+                                    out.println("[menu item(" + i + ")]: ");
+                                    OrderData.addOrderItem(input.nextLine());
+                                    Cls.cls();
+                                    out.println("Would you like add another item?");
+                                    out.print("[(Y)es/(N)o]: ");
+                                    ///wait I got confused here...need to use function to pass all order items to orderItem list or hashmap
+                                    // then create object, then pass it to the hashmap
+                                    String orderList = OrderData.orderItems(); //telling me to make something static here
+                                     //this *should* add order object to the existing order hashmap
+                                     orderData.addOrder(Order); ////wtf??
+                                    i++; //augments the menu entry item that precedes [menu item(#)
+                                    orderNum++; //advances orderNum - this variable is outside the loop and should keep advancing
+                                    restart = YesNo.yesNo(input.nextLine());
+
+                                } while (restart);
+
+                        case 2 -> {
+                                  boolean restart;
+                                  Cls.cls();
+                                  out.println("Enter the number of the order to modify: ")
+                                            //need option to list orders so they can see order numbers - listAll() method in order data
+                                    choice = input.nextInt();
+                                    //print the getters for the info for this order
+                                    //what would you like to change?  choice will delete entry....\
+                                    //delete that entry with 'set' if it's an array list or something else if hash'
+                                    // menu to select
+                            boolean as above for modification finished?
+                        }
+
+                        case 3 -> { // this one is list all orders...iterate through hash or arraylist - just listAll from orderData
+                                    boolean restart;
+                                    Cls.cls();
+                                    //goes iterates through hashmap get and printing info for each item
+
+
+                        }
+                        case 4 -> { // this one is delete order - listAll(), then orderData.remove<orderNum, order>
+
+                            {
+                        case 5 -> { // this one is return to main menu
+
+                        }
+                            }
+                        }
+
+
+
+                                }
+
+
+                                }
+                                }
+                                }
+                            }
+                        }
+
+                    //default -> out.println("Invalid choice.");
+
+                        //remember to put the assignNum() method in the constructor at the end of 'create ordery/n'
