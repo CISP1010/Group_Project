21a5c1af5eb945aaef940fe5ec21d630f9b7b0da
gclark2 <gclark2@my.chattanoogastate.edu>
2023-04-23 18:06:26 -0400

    did java documentation for the order constructor class

--- src/Orders/Order.java
+++ src/Orders/Order.java
@@ -5,10 +5,16 @@ import Menu.MenuData;
 import java.util.ArrayList;
 
 /**
- * @todo Order.java needs all javadoc info added
- * @body add all java doc info (Chat gpt)
+ *  The Order class creates an object that represents a food order with parameters for information
+ *   about the customer, the order itself, and the delivery details.
  */
+
+
 public class Order {
+    /**
+     * These are private instance variable for the delivery address, phone #, notes, table #
+     * customer name, order item, and the order type (dine in, delivery, or take away.
+     */
     private String deliveryAddress;
     private String deliveryPhone;
     private String deliveryNotes;
@@ -17,8 +23,24 @@ public class Order {
     private String custName;
     private ArrayList<Integer> orderItem;
     private String orderType;
+    /**
+     * creates a hashmap that contains the offered menu items
+     */
     MenuData menuData = new MenuData();
 
+    /**
+     * creates an order object with parameters for the order number, customer name, items in the order
+     * the type of arder, the table number - if dining in - , the delivery address and phone # -
+     * if for delivery, and any notes.
+     * @param orderNum contains the order number
+     * @param custName contains the customer name
+     * @param orderItems contains the order's items
+     * @param orderType indicates whether the order is to be served in-house
+     * @param tableNum takes the table number, if applicable
+     * @param deliveryAddress takes the deliver address if applicable
+     * @param deliveryPhone takes the deliver phone number if applicable
+     * @param deliveryNotes takes any miscellaneous notes any notes for the delivery (e.g. gate code, special instructions)
+     */
     public Order(int orderNum, String custName, ArrayList<Integer> orderItems, String orderType, int tableNum, String deliveryAddress, String deliveryPhone, String deliveryNotes) {
         this.orderNum = orderNum;;
         this.custName = custName;
@@ -30,62 +52,112 @@ public class Order {
         this.deliveryNotes = deliveryNotes;
     }
 
-    //setters
+    /**
+     * this is a setter for the order number
+     * @param orderNum takes the order number
+     */
     public void setNum(int orderNum) {
         this.orderNum = orderNum;
     }
 
+    /**
+     * this is a setter for the customer name
+     * @param custName takes the customer name
+     */
     public void setCustName(String custName) {
         this.custName = custName;
     }
 
+    /**
+     * this is a setter for the order items arraylist
+     * @param orderItems this is an array list that takes ordered items
+     */
     public void setOrderItem(ArrayList<Integer> orderItems){
         this.orderItem = orderItems;
 
     }
 
+    /**
+     * this is a setter for the order type
+     * @param orderType contains information about whether the meal is to be eaten in, delivered, or picked up
+     */
     public void setOrderType(String orderType){
         this.orderType = orderType;
     }
 
-    //getters
+    /**
+     * this is a getter for the order number
+     * @return it returns the order number
+     */
 
     public int getOrderNumber(){
         return orderNum;
     }
 
+    /**
+     * this is a getter for the delivery address
+     * @return it returns the delivery address
+     */
     public String getDeliveryAddress(){
         return deliveryAddress;
     }
 
+    /**
+     * gets the customer phone number
+     * @return returns the customer phone number
+     */
     public String getPhone(){
         return deliveryPhone;
     }
 
+    /**
+     * gets the delivery notes
+     * @return returns the deliver notes
+     */
     public String getDeliveryNotes(){
         return deliveryNotes;
     }
 
+    /**
+     * gets the table number
+     * @return returns the table number
+     */
     public int getTableNum(){
         return tableNum;
     }
 
+    /**
+     * gets the customer name
+     * @return returns the customer name
+     */
     public String getCustName(){
         return custName;
     }
 
-    public String getOrderItems(){ //this is returning the arraylist of order items as a string
+    /**
+     * This builds a list of the ordered item and returns that list as a string
+     * @return returns the build string of ordered items from the menu item hashmap of existing menu items
+     */
+    public String getOrderItems(){
         StringBuilder sb = new StringBuilder();
-        for (Integer itemNum : orderItem) { //for each item in the orderItem arraylist
-            sb.append(menuData.getName(itemNum)).append(", "); //print the item name from the menuData hashmap
+        for (Integer itemNum : orderItem) {
+            sb.append(menuData.getName(itemNum)).append(", ");
         }
         return sb.toString();
     }
 
+    /**
+     * this gets the ordertype
+     * @return returns the ordertype
+     */
     public String getOrderType(){
         return orderType;
     }
 
+    /**
+     * this is a utility function to automatically increment the order number
+     * @return returns the order number
+     */
     public int newOrderNum(){
         orderNum++;
         return orderNum;
